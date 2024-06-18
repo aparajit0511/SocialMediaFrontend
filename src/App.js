b41741e8e0cfd3261c0c3c89f490ccb87606cf86
aparajit0511 <aparajit35@gmail.com>
2024-06-19 00:13:36 +0530

    adding routes to home and styles

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { Paper, Container, Box, CssBaseline, Grid } from "@mui/material";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Box
+        sx={{
+          width: "100vw",
+          height: "100vh",
+          display: "flex",
+          alignItems: "flex-start",
+          justifyContent: "center",
+          backgroundColor: "background.default",
+          padding: 0, // Ensure no padding from the Box component
+          margin: 0,
+        }}
+      >
+        <Container
+          maxWidth="lg" // Ensure the container takes full width up to a reasonable limit
+          sx={{
+            padding: 0, // Ensure no padding from the Container component
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Grid>
+            <Grid item xl={12}>
+              <Paper
+                elevation={10}
+                sx={{
+                  width: "100%", // Match the width of the parent container
+                  padding: 2, // Add some padding for better appearance
+                  fontFamily: "Arial, sans-serif", // Customize the font family
+                  fontSize: "1.2rem", // Customize the font size
+                  textAlign: "center", // Center the text
+                }}
+              >
+                Social Media App
+              </Paper>
+            </Grid>
+          </Grid>
+        </Container>
+      </Box>
+    </ThemeProvider>
   );
 }
 
